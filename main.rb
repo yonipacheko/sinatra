3383fdf400b25e667d4d73988ecfac1708ee3726
yoni pacheko <fernando.lbarrajon8@gmail.com>
2013-07-31 11:13:05 +0200

    done with the app

--- main.rb
+++ main.rb
@@ -5,13 +5,16 @@ require 'pry'
 IMAGES = [
     {title: 'utopia', url: 'http://media.tumblr.com/ccee6264ec66e2b95575fabb758ab6e7/tumblr_inline_mq2vu61W0F1qz4rgp.png'},
     {title: 'druids', url: 'http://24.media.tumblr.com/b037901f8c302fc4368432a102f16442/tumblr_miypxbYWoA1s4040oo1_400.gif'},
-    {title: 'purple snow', url: 'http://24.media.tumblr.com/75597f75cec43fa621fa8f188ea7ce6e/tumblr_mnb1e14emK1r2lohvo1_500.jpg'}
+    {title: 'purple snow', url: 'http://24.media.tumblr.com/75597f75cec43fa6BLACKJACK_AMOUNTfa8f188ea7ce6e/tumblr_mnb1e14emK1r2lohvo1_500.jpg'}
 ]
 
 
 
 set :sessions, true
 
+BLACKJACK_AMOUNT = 21
+DEALER_MININUM_AMOUNT = 17
+INITIAL_POT_AMOUNT = 500
 
 helpers do
 
@@ -37,7 +40,7 @@ helpers do
 
     #correct for Aces
     arr.select{|e| e == "A"}.count.times do
-      total -= 10 if total > 21
+      total -= 10 if total > BLACKJACK_AMOUNT
     end
     total
   end #ends calculating_the_total
@@ -78,10 +81,12 @@ helpers do
 
 
   def do_we_have_right_value(value)
-    if 0 == value.to_i || value.to_i > 500
-      @error = "This amount should not be 0 and should not be more than 500."
+
+    if 0 == value.to_i || value.to_i > INITIAL_POT_AMOUNT
+      @error = "This amount should not be $0 and should not be more than $#{INITIAL_POT_AMOUNT}."
       halt erb :betting_page
     end
+
   end  # ends do_we_have_right_value
 
   def updating_player_pot(value, switcher)
@@ -107,25 +112,34 @@ get '/' do
 
     # progress to the game
     redirect '/game'
-    p 'OK'
   else
     redirect '/new_player'
   end
 end
 
 get '/new_player' do
-  session[:player_pot] = 500
+  session[:player_pot] = INITIAL_POT_AMOUNT
   erb :new_player
 end
 
 
 post '/new_player' do
-  session[:player_name] = params[:player_name]
+
+  session[:player_name] = params[:player_name].capitalize
+  if session[:player_name].empty? || session[:player_name] =~ /\d/
+    @error = "Try again!, you need to enter a proper name, thank you"
+    halt erb :new_player
+
+  end
   redirect '/betting_page'
 end
 
 get '/betting_page' do
+  if session[:player_pot] == 0  # check-point, if the player has some money left?
+    redirect '/no_game'
+  end
   session[:betting_amount] = nil
+
   erb :betting_page
 end
 
@@ -135,6 +149,11 @@ post '/betting_page' do
   #binding.pry
 
   redirect '/game'
+end
+
+get '/no_game' do
+
+  erb :no_game
 end
 
 get '/game' do
@@ -169,9 +188,14 @@ post '/game/player/hit' do
   session[:player_cards] << session[:deck].pop
   #binding.pry
   player_total = calculating_the_total(session[:player_cards])
-  if player_total == 21
-    @success = "Congra #{session[:player_name]}, U win, Yr total amount is #{updating_player_pot(session[:betting_amount], true)} !"
-  elsif player_total > 21
+
+  if player_total == BLACKJACK_AMOUNT
+    @showing_modal = true
+    @success = "Congra #{session[:player_name]}, U win, Your total amount is #{updating_player_pot(session[:betting_amount], true)} !"
+    @main_btn_visible = false
+
+  elsif player_total > BLACKJACK_AMOUNT
+    @showing_modal = true
     @error = "Sorry, but it seems like #{session[:player_name]} loses, You have #{updating_player_pot(session[:betting_amount], false)}."
     @main_btn_visible = false
   end
@@ -183,10 +207,9 @@ end # ends hit:btn
 post '/game/player/stay' do
 
   @success = "#{session[:player_name]} has chosen to stay."
-  @main_btn_visible = false
+  #@main_btn_visible = false
 
   redirect '/game/dealer'
- # erb :game
 
 end # ends stay:btn
 
@@ -198,13 +221,16 @@ get '/game/dealer' do
 
   dealer_total = calculating_the_total( session[:dealer_cards] )
 
-  if dealer_total == 21
-    @success = "Dealer hit blackjack!, your pont amount is #{updating_player_pot(session[:betting_amount], false)} "
+  if dealer_total == BLACKJACK_AMOUNT
+    @showing_modal = true
+    @success = "Dealer hit blackjack!, your current pot-amount is #{updating_player_pot(session[:betting_amount], false)} "
 
-  elsif dealer_total > 21
-    @error = "Dealer is busted, your pot amount is #{updating_player_pot(session[:betting_amount], true)}"
+  elsif dealer_total > BLACKJACK_AMOUNT
+    @showing_modal = true
 
-  elsif dealer_total  >= 17
+    @error = "Dealer is busted, your current pot-amount is #{updating_player_pot(session[:betting_amount], true)}"
+
+  elsif dealer_total  >= DEALER_MININUM_AMOUNT
     redirect '/game/dealer/compare'
   else
     @dealer_btn = true
@@ -229,17 +255,28 @@ get '/game/dealer/compare' do
   player_total = calculating_the_total( session[:player_cards])
 
   if dealer_total > player_total
-    @error = "Sorry Dealer wins. Dealer has #{dealer_total}. Your current pot amount is #{updating_player_pot(session[:betting_amount], false)
+    @showing_modal = true
+
+    @error = "Sorry Dealer wins. Your current pot-amount is #{updating_player_pot(session[:betting_amount], false)
     }"
   elsif  dealer_total < player_total
-    @success = "Congratualtions you win, Dealer has #{dealer_total}. Your current pot amount is #{updating_player_pot(session[:betting_amount], true)
+    @showing_modal = true
+
+    @success = "Congratualtions you win, Your current pot-amount is #{updating_player_pot(session[:betting_amount], true)
     }"
   else
+    @showing_modal = true
+
     @success = "It's a tie!!"
   end
 
   erb :game
 end
+
+get '/game_over' do
+  erb :game_over
+end
+
 
 
 
